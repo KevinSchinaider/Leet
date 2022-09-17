1b7b3df48821a8f59e9fba64791dcb1f5de388ac
KevinSchinaider <kevinschinaider05@outlook.com>
2022-09-16 22:02:28 -0300

    Solução do exercício

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Texto a ser traduzido: ");
+Console.WriteLine(
+    Console.ReadLine()
+    .Replace( "A", "4")
+    .Replace( "a", "4")
+    .Replace( "E", "3")
+    .Replace( "e", "3")
+    .Replace( "I", "3")
+);
